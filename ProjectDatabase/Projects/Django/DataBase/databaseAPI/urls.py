cdba6a516ea6aad75d78ef677c53a9e4adb56c02
Ali Ahmmed Tonoy <tonoy.sust@gmail.com>
2019-04-02 03:44:35 +0600

    api

---
+++ ProjectDatabase/Projects/Django/DataBase/databaseAPI/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from databaseAPI import views
+
+urlpatterns = [
+    path('', views.api_home_view, name='api_home'),
+]
--- ProjectDatabase/Projects/Django/DataBase/databaseAPI/views.py
+++ ProjectDatabase/Projects/Django/DataBase/databaseAPI/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def api_home_view(request):
+    return HttpResponse("<h2>API view.</h2>")
